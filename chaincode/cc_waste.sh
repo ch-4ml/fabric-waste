170a7ea812499f47ec414044c9f9fb9218b203f8
ch-4ml <fetch@ajou.ac.kr>
2021-11-28 03:16:23 +0900

    fix: Fix chaincode package name

--- chaincode/cc_waste.sh
+++ chaincode/cc_waste.sh
@@ -3,20 +3,20 @@
 set -ev
 
 #chaincode install
-docker exec cli peer chaincode install -n wasteapp -v 1.2 -p github.com/waste/go
+docker exec cli peer chaincode install -n waste -v 1.0 -p github.com/waste/go
 sleep 3
 
 #chaincode instatiate
-docker exec cli peer chaincode instantiate -n wasteapp -v 1.2 -C mychannel1 -c '{"Args":[]}' -P 'OR ("Org1MSP.member", "Org2MSP.member")'
+docker exec cli peer chaincode instantiate -n waste -v 1.0 -C mychannel1 -c '{"Args":[]}' -P 'OR ("Org1MSP.member", "Org2MSP.member")'
 sleep 5
 
 #chaincode invoke user1
-docker exec cli peer chaincode invoke -n wasteapp -C mychannel1 -c '{"Args":["initLedger"]}'
+docker exec cli peer chaincode invoke -n waste -C mychannel1 -c '{"Args":["initLedger"]}'
 sleep 5
 
 #chaincode query user1
 # Waste{ObjectType, Phase: args[1], Name: args[2], Color: args[3], CasNo: args[4]}
-docker exec cli peer chaincode query -n wasteapp -C mychannel1 -c '{"Args":["createWaste","4", "Gas", "Cyanogen chioride", "None", "506-77-4"]}'
+docker exec cli peer chaincode query -n waste -C mychannel1 -c '{"Args":["createWaste","4", "Gas", "Cyanogen chioride", "None", "506-77-4"]}'
 
 
 echo '-------------------------------------END-------------------------------------'
